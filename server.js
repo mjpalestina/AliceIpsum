83146865f5974592e7e3996f0f7265cc99928307
Marc Palestina <mjpalestina@gmail.com>
2017-08-22 12:04:23 -0400

    Setup for server.js for using handlebars

---
+++ server.js
@@ -0,0 +1,27 @@
+// DEPENDENCIES
+const request = require('request');
+const express = require('express');
+const bodyParser = require("body-parser");
+const exphbs = require('express-handlebars');
+
+// CREATE INSTANCE OF express APP
+const app = express();
+app.use(express.static(__dirname + "/public"));
+
+// SET HANDLEBARS
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var routes = require("./controllers/controllers.js");
+app.use("/", routes);
+
+// SPECIFY THE PORT
+const port = process.env.PORT || 4000;
+
+//PORT CALL
+app.listen(port, function() {
+	console.log("express app listening on port ", + port);
+  });
+
+//EXPORT
+module.exports = express;
