db270b11349daaa3aaf11f75ed98805c955f0480
Marc Palestina <mjpalestina@gmail.com>
2017-08-25 16:24:16 -0400

    Added clear and styling

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,16 +1,16 @@
 $(".quotebutton").click(function(){
 
+  $(".randomquote").empty();
+
   var amount = $('#quote_amount').val();
 
   for (i = 0; i < amount; i++ ) {
 
     var random_quote = quotes[Math.floor(Math.random()*quotes.length)];
 
-      $(".randomquote").append(random_quote);
+      $(".randomquote").append("<p>" + random_quote + "</p>");
 
       console.log(random_quote);
       console.log(amount);
   };
-
-
 });
