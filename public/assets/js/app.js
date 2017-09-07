031cc0b92258011b74c1f88964d48cc7542a2ab1
Marc Palestina <mjpalestina@gmail.com>
2017-09-07 19:41:51 -0400

    Cleaned code up, changed naming around

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -16,20 +16,29 @@ var aaiw = [];
 //Ipsum generation
 $(".makeAliceIpsum").click(function(){
 
-  //Emty previous ipsum
+  //Empty previous ipsum
   $(".AliceIpsumLocation").empty();
+
+  //Get amount
+  var amount = $('#ipsum_amount').val();
+    console.log("Amount", amount);
 
   //Get selected Type
   var output_type = $('#output_type').val();
+    console.log("Output Type:", output_type);
+
+  //Checks if tags are requested
+  var add_p_tag = $('#add_p_tag').prop('checked');
+    console.log("Paragraph Tags:", add_p_tag);
 
   //Get selected chapters
   var chapter = $('#aaiw_chapters').val();
 
-    //Error catch for no selection
-    if (chapter == null ) {
-        var chapter = ["aaiw_all"];
-      };
-        console.log("Chapters", chapter);
+  //Error catch for no selection
+  if (chapter == null ) {
+      var chapter = ["aaiw_all"];
+    };
+    console.log("Chapters:", chapter);
 
   //Push selected chapters into array
   if (chapter.includes("aaiw_c1") || chapter.includes("aaiw_all")) {
@@ -44,27 +53,28 @@ $(".makeAliceIpsum").click(function(){
       var ipsum = aaiw.concat(aaiw_chapter3);
     };
 
-//Returns set amount
-  var amount = $('#ipsum_amount').val();
-    console.log("Amount", amount);
 
 //Loop through amount to output random AliceIpsum
-
-
-  if (output_type.includes("quote_characters")) {
+  //If Characters is selected
+  if (output_type.includes("book_characters")) {
     var random_ipsum = ipsum[Math.floor(Math.random()*ipsum.length)];
     var character_ipsum = random_ipsum.substring(0, amount ++);
       console.log("character_ipsum:", character_ipsum);
     $(".AliceIpsumLocation").append(character_ipsum);
+        console.log("Character- random_ipsum:", random_ipsum);
 
-      } else {
+  } else {
+  //If Paragraph is selected
+  for (i = 0; i < amount; i++ ) {
+    var random_ipsum = ipsum[Math.floor(Math.random()*ipsum.length)];
 
-    for (i = 0; i < amount; i++ ) {
-      var random_ipsum = ipsum[Math.floor(Math.random()*ipsum.length)];
-        $(".AliceIpsumLocation").append("<p>" + random_ipsum + "</p>");
-      };
+    if (add_p_tag == true) {
+      var p_open = "open";
+      var p_close = "close";
     };
 
-  console.log(random_ipsum);
-
+      $(".AliceIpsumLocation").append("<br>", p_open, random_ipsum, p_close, + "<br>");
+        console.log("Chapter- random_ipsum:", random_ipsum);
+    };
+  };
 });
