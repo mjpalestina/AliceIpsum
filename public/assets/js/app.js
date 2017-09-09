fada633a74cd6c80c849093620ac2e5b58539774
Marc Palestina <mjpalestina@gmail.com>
2017-09-09 18:45:15 -0400

    Added clear button and swithed to prepend and remove clear on generate

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -10,14 +10,16 @@ $(".copybutton").click(function(){
     console.log("Copy Me- Clicked");
   });
 
+//Clear Button
+$(".clearAliceIpsum").click(function(){
+    $(".AliceIpsumLocation").empty();
+  });
+
 //Blank array to hold selection
 var aaiw = [];
 
 //Ipsum generation
 $(".makeAliceIpsum").click(function(){
-
-  //Empty previous ipsum
-  $(".AliceIpsumLocation").empty();
 
   //Get amount
   var amount = $('#ipsum_amount').val();
@@ -53,27 +55,26 @@ $(".makeAliceIpsum").click(function(){
       var ipsum = aaiw.concat(aaiw_chapter3);
     };
 
+//Check if Paragraph Tags are requested
+  if (add_p_tag == true) {
+    var p_open = "&lt;p&gt;";
+    var p_close = "&lt;/p&gt;";
+  };
 
 //Loop through amount to output random AliceIpsum
   //If Characters is selected
   if (output_type.includes("book_characters")) {
     var random_ipsum = ipsum[Math.floor(Math.random()*ipsum.length)];
     var character_ipsum = random_ipsum.substring(0, amount ++);
-      console.log("character_ipsum:", character_ipsum);
-    $(".AliceIpsumLocation").append(character_ipsum);
-        console.log("Character- random_ipsum:", random_ipsum);
+      console.log("Character- random_ipsum:", character_ipsum);
+    $(".AliceIpsumLocation").prepend(p_open, character_ipsum, p_close, " ");
 
   } else {
   //If Paragraph is selected
   for (i = 0; i < amount; i++ ) {
     var random_ipsum = ipsum[Math.floor(Math.random()*ipsum.length)];
 
-    if (add_p_tag == true) {
-      var p_open = "&lt;p&gt;";
-      var p_close = "&lt;/p&gt;";
-    };
-
-      $(".AliceIpsumLocation").append("<br>", p_open, random_ipsum, p_close, + "<br>");
+      $(".AliceIpsumLocation").prepend("<br>", p_open, random_ipsum, p_close, + "<br>");
         console.log("Chapter- random_ipsum:", random_ipsum);
     };
   };
